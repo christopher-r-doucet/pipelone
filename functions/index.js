e3d7b5321e4731ecdc10a65f7163cb2a334419aa
christopher-r-doucet <cdoucet6302@gmail.com>
2024-11-18 10:38:05 -0700

    update

--- functions/index.js
+++ functions/index.js
@@ -17,3 +17,12 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+exports.app = functions.https.onRequest(app);

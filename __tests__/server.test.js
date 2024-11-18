b73c14de01413d54196f108ebde953f768f2f5ef
christopher-r-doucet <cdoucet6302@gmail.com>
2024-11-18 09:55:54 -0700

    update

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,12 +1,15 @@
-describe('Server Tests', () => {
-    it('should return a 200 status code', async () => {
-        const response = await fetch('http://localhost:3001');
-        expect(response.status).toBe(200);
-    });
-    
-    it('should return the correct response body', async () => {
-        const response = await fetch('http://localhost:3001');
-        const data = await response.json();
-        expect(data).toEqual({ message: 'Hello, World!' });
-    });
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+describe('GET /', () => {
+  it('responds with Hello World!', (done) => {
+    request(app)
+      .get('/')
+      .expect('Hello World!', done);
+  });
 });
